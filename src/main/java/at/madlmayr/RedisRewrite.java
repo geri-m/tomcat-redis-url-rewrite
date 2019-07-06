41a1453cfef32809be8838b57cbe7e51c8df5b36
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2019-07-06 21:57:37 +0200

    Pass parameter to via XML attributes (#2)

--- src/main/java/at/madlmayr/RedisRewrite.java
+++ src/main/java/at/madlmayr/RedisRewrite.java
@@ -5,7 +5,8 @@ import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.RequestFilterValve;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
-import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
@@ -16,29 +17,37 @@ public class RedisRewrite extends RequestFilterValve {
 
     private String host = "localhost";
     private int port = 6379;
+    private int timeout = 100;
 
-    private static Jedis singleton;
-
-    public synchronized static Jedis getJedis(String host, int port){
-        if(singleton == null)
-            singleton = new Jedis(host, port);
-
-        return singleton;
-    }
+    private JedisPool pool;
 
     public RedisRewrite(){
         super();
-        log.debug("const");
+        log.debug("constructor");
+        createPool();
     }
 
     public void setHost(String host){
-        log.info("setHost" + host);
+        log.info("setHost: " + host);
         this.host = host;
+        createPool();
     }
 
     public void setPort(int port){
-        log.info("setPort" + port);
+        log.info("setPort: " + port);
         this.port = port;
+        createPool();
+    }
+
+    public void setTimeout(int timeout){
+        log.info("setTimeout: " + timeout);
+        this.timeout = timeout;
+        createPool();
+    }
+
+    private void createPool(){
+        log.info(String.format("create Pool: %s:%s, %s ms", host ,port , timeout));
+        pool = new JedisPool(new JedisPoolConfig(), host, port, timeout);
     }
 
     // --------------------------------------------------------- Public Methods
@@ -48,7 +57,7 @@ public class RedisRewrite extends RequestFilterValve {
         long start = System.currentTimeMillis();
         log.info("Start: " + request.getRequestURI());
 
-        getJedis(this.host, this.port).get("events/city/rome");
+        pool.getResource().get("events/city/rome");
 
         String property;
         if (getAddConnectorPort()) {
